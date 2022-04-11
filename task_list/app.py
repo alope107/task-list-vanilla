8bb1357fb09a84c287f5f8a406b72325cf2f4961
Auberon López <auberon@adadev.org>
2022-04-10 19:20:40 -0700

    Created simple_psyco library.
    - Made small library to handle psycopg2 connections and cursors.
    
    - Refactored Goal model to use simple_psyco.

--- task_list/app.py
+++ task_list/app.py
@@ -4,13 +4,10 @@ from flask import Flask
 
 from task_list.models.goal import Goal
 from task_list.routes.goal import goal_bp
+from task_list.simple_psyco.database import Database
 
-import psycopg2
-
-# TODO(auberon): Investigate connection pool.
-db = psycopg2.connect("dbname='vanilla' user='postgres'")
+db = Database("vanilla", "postgres")
 Goal.db = db
-atexit.register(db.close)
 
 app = Flask(__name__)
 app.register_blueprint(goal_bp)
--- task_list/models/goal.py
+++ task_list/models/goal.py
@@ -8,25 +8,19 @@ class Goal:
 
     @classmethod
     def create_and_insert(cls, title, due_date=None):
-        cur = cls.db.cursor()
-        cur.execute("""INSERT INTO Goal(title, due_date)
-                       VALUES (%s, %s) 
-                       RETURNING id""", (title, due_date))
-        id = cur.fetchone()[0]
-        cls.db.commit()
-        cur.close()
+        result = Goal.db.query("""INSERT INTO Goal(title, due_date)
+                                  VALUES (%s, %s) 
+                                  RETURNING id""", (title, due_date))
+        id = result[0][0]
         return cls(id, title, due_date)
 
     @classmethod
     def fetch_all(cls):
-        cur = cls.db.cursor()
-        cur.execute("""SELECT id, title, due_date
-                       FROM Goal""")
-        rows = cur.fetchall()
+        rows = Goal.db.query("""SELECT id, title, due_date
+                                  FROM Goal""")
         goals = []
         for id, title, due_date in rows:
             goals.append(cls(id, title, due_date))
-        cur.close()
         return goals
 
     def __repr__(self):
