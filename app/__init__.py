80f772f8ddb7d54b17f333905caf10ea711ac886
Auberon López <auberon@adadev.org>
2022-04-16 10:42:59 -0700

    Load config from .env.
    - Load database name and user from .env file (not included).

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 from app.models.goal import Goal
 from app.routes.goal import goal_bp
 from app.simple_psyco.database import Database
 
-def create_app(test_config=None):
-    db = Database("vanilla", "postgres")
+load_dotenv()
+config = {
+    "dbname": os.environ.get("APP_DBNAME"),
+    "user": os.environ.get("APP_USER")
+}
+
+# TODO(auberon): Investigate config.from_pyfile
+def create_app(config=config):
+    db = Database(config["dbname"], config["user"])
     Goal.db = db
 
     app = Flask(__name__)
